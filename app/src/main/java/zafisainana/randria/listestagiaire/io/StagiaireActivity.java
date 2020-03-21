ffa25b563bdb42d46cdcefceab75c1d1a1a45d15
tianaos <zafisainana.randria.o@gmail.com>
2020-03-21 19:38:03 +0100

    ajout adapter

---
+++ app/src/main/java/zafisainana/randria/listestagiaire/io/StagiaireActivity.java
@@ -0,0 +1,29 @@
+package zafisainana.randria.listestagiaire.io;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class StagiaireActivity extends AppCompatActivity {
+
+    String Nom_S[], Adresse_S[], Email_S[];
+    int images[] = {R.drawable.avatar,R.drawable.avatar,R.drawable.avatar,R.drawable.avatar,R.drawable.avatar,R.drawable.avatar,R.drawable.avatar,R.drawable.avatar,R.drawable.avatar,R.drawable.avatar,R.drawable.avatar,R.drawable.avatar,
+            R.drawable.avatar,R.drawable.avatar,R.drawable.avatar};
+    RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stagiaire);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        Nom_S = getResources().getStringArray(R.array.Nom_Stagiaire);
+        Adresse_S = getResources().getStringArray(R.array.Adresse_Stagiaire);
+        Email_S = getResources().getStringArray(R.array.Email_Stagiaire);
+
+        MyAdapter myAdapter = new MyAdapter(this, Nom_S, Adresse_S,Email_S,images);
+    }
+}
