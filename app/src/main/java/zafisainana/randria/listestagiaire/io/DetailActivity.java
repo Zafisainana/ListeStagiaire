a3124f3db25952898b64f74ff19fccd0506b7032
tianaos <zafisainana.randria.o@gmail.com>
2020-03-24 11:37:22 +0100

    il manquait un s

--- app/src/main/java/zafisainana/randria/listestagiaire/io/DetailActivity.java
+++ app/src/main/java/zafisainana/randria/listestagiaire/io/DetailActivity.java
@@ -7,15 +7,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 public class DetailActivity extends AppCompatActivity {
 
     ImageView MainImageView;
-    TextView Nom_Txt, Adresse_Txt, Email_Txt;
+    TextView myNom, myAdresse, myEmail;
 
     String data1,data2,data3;
-    int myImage;
+    int myImages;
 
 
     @Override
@@ -24,22 +22,23 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         MainImageView = findViewById(R.id.MainImageView);
-        Nom_Txt = findViewById(R.id.Nom_Txt);
-        Adresse_Txt = findViewById(R.id.Adresse_Txt);
-        Email_Txt = findViewById(R.id.Email_txt);
+        myNom = findViewById(R.id.MyName);
+        myAdresse = findViewById(R.id.MyAdresse);
+        myEmail = findViewById(R.id.MyEmail);
 
-        //getData();
-        //setData();
+        getData();
+        setData();
     }
     private void getData(){
 
         // on vérifie qu'il y a des données pour renvoyer la vue
-        if (getIntent().hasExtra("myImage") && getIntent().hasExtra("data1") &&
+        if (getIntent().hasExtra("myImages") && getIntent().hasExtra("data1") &&
                 getIntent().hasExtra("data2")&& getIntent().hasExtra("data3")){
+
             data1=getIntent().getStringExtra("data1");
             data2=getIntent().getStringExtra("data2");
             data3=getIntent().getStringExtra("data3");
-            myImage=getIntent().getIntExtra("myImage",1);
+            myImages=getIntent().getIntExtra("myImages",1);
 
         }else{
             // si pas de data
@@ -50,10 +49,10 @@ public class DetailActivity extends AppCompatActivity {
     }
     private void setData(){
 
-        Nom_Txt.setText(data1);
-        Adresse_Txt.setText(data2);
-        Email_Txt.setText(data3);
-        MainImageView.setImageResource(myImage);
+        myNom.setText(data1);
+        myAdresse.setText(data2);
+        myEmail.setText(data3);
+        MainImageView.setImageResource(myImages);
 
 
     }
